461a06ee436ffef8677442a3b766f01112ac02c1
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-01 17:19:49 -0300

    feat: check if product is valid

--- internal/application/product.go
+++ internal/application/product.go
@@ -1,12 +1,23 @@
 package application
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	Enable()
+	Disable()
+	IsValid()
 }
 
 type Product struct {
+	ID     string
 	Name   string
 	Price  float64
 	Status string
@@ -31,4 +42,20 @@ func (p *Product) Disable() error {
 		return nil
 	}
 	return errors.New("the prive must be zero in order to have the product disabled")
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("the status must be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("the price must be greater or equal zero")
+	}
+
+	return true, nil
 }
