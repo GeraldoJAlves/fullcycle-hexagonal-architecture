ef4b961989b5753e05fa59bce027eb01f54cf826
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-01 17:07:23 -0300

    test: allow disable product when price is equal zero

--- internal/application/product_test.go
+++ internal/application/product_test.go
@@ -27,3 +27,24 @@ func Test_ProductEnabled(t *testing.T) {
 	require.Equal(t, application.DISABLED, p.Status)
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
 }
+
+func Test_ProductDisabled(t *testing.T) {
+	p := application.Product{
+		Name:   "Ball",
+		Price:  0,
+		Status: application.ENABLED,
+	}
+
+	err := p.Disable()
+
+	require.Nil(t, err)
+	require.Equal(t, application.DISABLED, p.Status)
+
+	p.Status = application.ENABLED
+	p.Price = 10
+
+	err = p.Disable()
+	require.Equal(t, application.ENABLED, p.Status)
+	require.NotNil(t, err)
+	require.Equal(t, "the prive must be zero in order to have the product disabled", err.Error())
+}
