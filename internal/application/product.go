6234da24ca92c9a9fa7686a4059c436396719c16
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-02 20:25:54 -0300

    feat: add product's interface

--- internal/application/product.go
+++ internal/application/product.go
@@ -17,6 +17,21 @@ type ProductInterface interface {
 	IsValid()
 }
 
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(name string, price float64) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
