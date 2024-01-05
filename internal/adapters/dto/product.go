550c805d16b7a680165ade44447c6b47f4fedd3f
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-05 02:42:41 -0300

    feat: add create product route web server

---
+++ internal/adapters/dto/product.go
@@ -0,0 +1,28 @@
+package dto
+
+import "github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/application"
+
+type Product struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+	Status string  `json:"status"`
+}
+
+func NewProduct() *Product {
+	return &Product{}
+}
+
+func (p *Product) Bind(product *application.Product) (*application.Product, error) {
+	if p.ID != "" {
+		product.ID = p.ID
+	}
+
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = p.Status
+	if _, err := product.IsValid(); err != nil {
+		return &application.Product{}, err
+	}
+	return product, nil
+}
--- internal/adapters/web/handler/product.go
+++ internal/adapters/web/handler/product.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/codegangsta/negroni"
+	"github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/adapters/dto"
 	"github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/application"
 	"github.com/gorilla/mux"
 )
@@ -13,6 +15,9 @@ func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, s application.Produc
 	r.Handle("/products/{id}", n.With(
 		negroni.Wrap(getProduct(s)),
 	)).Methods("GET", "OPTIONS")
+	r.Handle("/products", n.With(
+		negroni.Wrap(createProduct(s)),
+	)).Methods("POST", "OPTIONS")
 }
 
 func getProduct(s application.ProductServiceInterface) http.HandlerFunc {
@@ -34,6 +39,40 @@ func getProduct(s application.ProductServiceInterface) http.HandlerFunc {
 			return
 		}
 
-		// w.WriteHeader(http.StatusOK)
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func createProduct(s application.ProductServiceInterface) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		var productDto = dto.NewProduct()
+
+		if err := json.NewDecoder(r.Body).Decode(&productDto); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		// var product = application.Product{}
+		// if _, err := productDto.Bind(&product); err != nil {
+		// 	w.WriteHeader(http.StatusBadRequest)
+		// 	jsonError(err.Error())
+		// 	return
+		// }
+
+		product, err := s.Create(productDto.Name, productDto.Price)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			fmt.Println(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+		}
 	}
 }
