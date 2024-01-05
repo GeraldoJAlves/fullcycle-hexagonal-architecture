0584554aac4a2efa701732bee1380daa84d07155
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-04 23:47:34 -0300

    test: enable cli product

--- internal/adapters/cli/product_test.go
+++ internal/adapters/cli/product_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRun(t *testing.T) {
+func TestRun_Create(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -31,8 +31,29 @@ func TestRun(t *testing.T) {
 	result, err := cli.Run(serviceMock, "create", "", productName, productPrice)
 
 	require.Nil(t, err)
-
 	require.Equal(t, fmt.Sprintf("Product ID %s with the name %s has been create with the price %f",
 		productId, productName, productPrice), result)
+}
 
+func TestRun_Enable(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	productName := "Ball 2"
+	productPrice := 2.99
+	productId := "uuid-1"
+
+	productMock := mock_application.NewMockProductInterface(ctrl)
+	productMock.EXPECT().GetID().Return(productId).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
+	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+
+	serviceMock := mock_application.NewMockProductServiceInterface(ctrl)
+	serviceMock.EXPECT().Get(gomock.Any()).Return(productMock, nil)
+	serviceMock.EXPECT().Enable(gomock.Any()).Return(productMock, nil)
+
+	result, err := cli.Run(serviceMock, "enable", "", productName, productPrice)
+
+	require.Nil(t, err)
+	require.Equal(t, fmt.Sprintf("Product %s has been enabled", productName), result)
 }
