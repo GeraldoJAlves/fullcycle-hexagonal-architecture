ed919eef0a39e35009fb1fbd92e94b7353a5ff1f
Geraldo Alves <junior.alves.2007@gmail.com>
2024-01-04 23:44:10 -0300

    test: create cli product

--- internal/adapters/cli/product.go
+++ internal/adapters/cli/product.go
@@ -6,7 +6,7 @@ import (
 	"github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/application"
 )
 
-func Run(service application.ProductService, action string, productId string, productName string, productPrice float64) (string, error) {
+func Run(service application.ProductServiceInterface, action string, productId string, productName string, productPrice float64) (string, error) {
 	var result string = ""
 
 	switch action {
---
+++ internal/adapters/cli/product_test.go
@@ -0,0 +1,38 @@
+package cli_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/adapters/cli"
+	mock_application "github.com/geraldojalves/fullcycle-hexagonal-architecture/internal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	productName := "Ball 2"
+	productPrice := 2.99
+	productStatus := "enabled"
+	productId := "uuid-1"
+
+	productMock := mock_application.NewMockProductInterface(ctrl)
+	productMock.EXPECT().GetID().Return(productId).AnyTimes()
+	productMock.EXPECT().GetName().Return(productName).AnyTimes()
+	productMock.EXPECT().GetPrice().Return(productPrice).AnyTimes()
+	productMock.EXPECT().GetStatus().Return(productStatus).AnyTimes()
+
+	serviceMock := mock_application.NewMockProductServiceInterface(ctrl)
+	serviceMock.EXPECT().Create(gomock.Any(), gomock.All()).Return(productMock, nil)
+
+	result, err := cli.Run(serviceMock, "create", "", productName, productPrice)
+
+	require.Nil(t, err)
+
+	require.Equal(t, fmt.Sprintf("Product ID %s with the name %s has been create with the price %f",
+		productId, productName, productPrice), result)
+
+}
